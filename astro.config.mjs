cd0c285460a5a1baafab50efa215b2b2684a3221
Nathaniel Ramm <nathaniel.ramm@discretedatascience.com>
2024-09-19 21:07:44 +1000

    Update deployment workflow and Astro configuration, remove unused code, and clean up package.json. Update deployment workflow and Astro configuration, remove unused code, and clean up package.json.
    - Removed unused code related to GitHub repo name and branch
    - Updated base URL in Astro configuration
    - Cleaned up unnecessary comments in package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,21 +4,10 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
-// const githubRepoName = 'mountainash-web';
-// const baseSegments = [githubRepoName];
-
-// // Use GITHUB_REF_NAME if available (in GitHub Actions), otherwise use a default or leave it empty
-// const branchName = process.env.GITHUB_REF_NAME || '';
-
-// if (branchName && branchName !== 'main') {
-//   baseSegments.push(branchName);
-// }
-
-// const base = `/${baseSegments.join('/')}`;
 
 // https://astro.build/config
 export default defineConfig({
   site: "https:///mountainash-io.github.io",
-  base: 'mountainash-web',  
+  base: "/mountainash-web",  
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
