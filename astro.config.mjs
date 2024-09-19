43383c203ca8e268d528ec0245b87d1c175aea93
Nathaniel Ramm <nathaniel.ramm@discretedatascience.com>
2024-09-19 21:49:38 +1000

    Remove unused workflow triggers, update site base URL, and refine content in components. Updated product links in pricing page. Remove unused workflow triggers, update site base URL, and refine content in components. Updated product links in pricing page.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
-
-// https://astro.build/config
 export default defineConfig({
   site: "https:///mountainash-io.github.io",
   base: "/mountainash-web",  
